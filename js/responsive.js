bafa4eeb37e92eda71fe70890f3a5dc33356197c
ismaelmours <monnapuleismaelmours@gmail.com>
2024-10-17 11:14:13 +0200

    made few changes

--- js/responsive.js
+++ js/responsive.js
@@ -1,7 +1,15 @@
-// script.js
+// JavaScript to handle menu toggle
 const mobileMenu = document.getElementById('mobile-menu');
 const navList = document.querySelector('.nav-list');
+const closeMenu = document.getElementById('close-menu');
 
 mobileMenu.addEventListener('click', () => {
-    navList.classList.toggle('active'); // Toggle the 'active' class
+    navList.classList.toggle('active');
+    closeMenu.style.display = 'block'; // Show the close button
 });
+
+closeMenu.addEventListener('click', () => {
+    navList.classList.remove('active');
+    closeMenu.style.display = 'none'; // Hide the close button
+});
+
